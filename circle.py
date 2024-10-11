ff37a26019b7ed87eba0e5b2f2079749ff6322f7
Ivan Deryagin <ivanderagin30@gmail.com>
2024-10-11 23:57:02 +0300

    добвлены комментарии к функциям

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+'''принимает число r,возращает площадь оркудности радиуса r'''
     return math.pi * r * r
 
 
 def perimeter(r):
+'''принимает число r,возращает перимитр круга радиуса r'''
     return 2 * math.pi * r
 
