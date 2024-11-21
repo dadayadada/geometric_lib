5456ea9398d28c1cd38c064ca6c9f07634238e44
dadayadada <ivanderagin30@gmail.com>
2024-11-21 16:31:06 +0300

    Update tests_square.py

--- tests/tests_square.py
+++ tests/tests_square.py
@@ -1,6 +1,7 @@
 import unittest
 from square import area, perimeter
 
+
 class TestSquare(unittest.TestCase):
 
     def test_area_positive_side(self):
@@ -24,6 +25,7 @@ class TestSquare(unittest.TestCase):
     def test_perimeter_negative_side(self):
         with self.assertRaises(ValueError):
             perimeter(-5)
+
 
 if __name__ == "__main__":
     unittest.main()
